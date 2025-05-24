c6eb153d998d243f312825427297b06fd5b23c1e
Devansh Raval <ravaldevansh01@gmail.com>
2025-05-24 10:24:04 +0530

    fixed footer, home page added slider and category

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -6,5 +6,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
-
+  options = {
+    loop: true,
+    margin: 10,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    smartSpeed: 2000, // ⬅️ controls transition speed in ms (e.g. 600ms)
+    dots: true,
+    nav: false,
+    animateOut: 'fadeOut',   // optional
+    animateIn: 'fadeOut',     // optional
+    responsive: {
+      0: { items: 1 },
+      600: { items: 1 },
+      1000: { items: 1 }
+    }
+  };
+  
+  images = [
+    { id: '1', src: 'assets/hero-banner-appliances.png', alt: 'Slide 1' },
+    { id: '2', src: 'assets/hero_microwaves.png', alt: 'Slide 2' },
+    { id: '3', src: 'assets/hero-banner-appliances.png', alt: 'Slide 3' }
+  ];
 }
